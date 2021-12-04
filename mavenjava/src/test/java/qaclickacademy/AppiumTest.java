5d7afeaa309451aa069116b6cad2d419174df02f
Arpitha <arpitham337@gmail.com>
2021-12-04 19:37:42 +0530

    firstcode

---
+++ mavenjava/src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void nativeapp()
+	{
+		System.out.println("nativeapp");
+	}
+	@Test
+	public void iosapp()
+	{
+		System.out.println("iosapp");
+	}
+}
---
+++ mavenjava/src/test/java/qaclickacademy/seleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+	@Test
+	public void browseraut()
+		{
+			System.out.println("browseraut");
+		}
+		@Test
+		public void elementsui()
+		{
+			System.out.println("elementsui");
+		}
+
+
+}
